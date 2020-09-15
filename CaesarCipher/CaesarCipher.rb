a61abf49200ac5b7ba73c46933ca7c29e60da50b
boss078 <36530564+boss078@users.noreply.github.com>
2020-09-15 20:18:22 +0300

    Added functions for encryption/decryption for more comfortably usage

--- CaesarCipher/CaesarCipher.rb
+++ CaesarCipher/CaesarCipher.rb
@@ -1,6 +1,19 @@
+# Usage:
+# Methods encrypt/decrypt for encryption/decryption
+# First arg - offset for cipher
+# Second arg - input string or input file opened in reading mode
+# Third arg(optional) - name for output file
+
 class CaesarCipher
-    def perform (mode, k, input, *args)
-        return nil if mode !~ /e|d/
+    def encrypt(k, input, *args)
+        return perform_something(true, k, input, args)
+    end
+    def decrypt(k, input, *args)
+        return perform_something(false, k, input, args)
+    end
+
+    private
+    def perform_something(encrypt, k, input, args)
         input_str = ''
         if input.class == File
             if input
@@ -16,7 +29,7 @@ class CaesarCipher
         
         input_str.each_char do |char|
             if char =~ /[a-z]|[A-Z]/
-                if mode == 'e'
+                if encrypt == true
                     modified_char = char.ord + k
                 else
                     modified_char = char.ord - k
@@ -33,18 +46,17 @@ class CaesarCipher
                 output_str += char
             end
         end
-        if args.length == 0
-            return output_str
-        else
+        if args.length != 0
             output_file = File.open(args[0], 'w')
             output_file.write(output_str)
             output_file.close
             return 0
         end
+        return output_str
     end
 end
 
+# Some test code
 cipherInstance = CaesarCipher.new
 inputFile = File.open('CaesarCipherDecrypted.txt')
-cipherInstance.perform('e', 2, inputFile, 'CaesarCipherEncrypted.txt')
-
+cipherInstance.encrypt('e', 2, inputFile, 'CaesarCipherEncrypted.txt')
