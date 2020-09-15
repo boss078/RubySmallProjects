765fa55206188f15bfff94576e758bb16e88c5a1
boss078 <36530564+boss078@users.noreply.github.com>
2020-09-15 14:52:47 +0300

    You can encrypt/decrypt string/files using CaesarCipher singletone

--- CaesarCipher/CaesarCipher.rb
+++ CaesarCipher/CaesarCipher.rb
@@ -1,57 +1,50 @@
-mode = ""
-until mode =~ /[1-2]/ do
-    print "Choose mode(1 - encrypt, 2 - decript): "
-    mode = gets.strip
-    case mode
-    when "1"
-        puts "Reading data from CaesarCipherDecrypted.txt..."
-        input_file = File.open("CaesarCipherDecrypted.txt")
-        input_str = input_file.read
-        input_file.close
-    when "2"
-        puts "Reading data from CaesarCipherEncrypted.txt..."
-        input_file = File.open("CaesarCipherEncrypted.txt")
-        input_str = input_file.read
-        input_file.close
-    else
-        puts "Wrong input"
+class CaesarCipher
+    def perform (mode, k, input, *args)
+        return nil if mode !~ /e|d/
+        input_str = ''
+        if input.class == File
+            if input
+                input_str = input.read
+                input.close
+            else
+                return nil
+            end
+        elsif input.class == String
+            input_str = input
+        end
+        output_str = ''
+        
+        input_str.each_char do |char|
+            if char =~ /[a-z]|[A-Z]/
+                if mode == 'e'
+                    modified_char = char.ord + k
+                else
+                    modified_char = char.ord - k
+                end
+                if char =~ /[a-z]/
+                    modified_char -= 26 if modified_char > 'z'.ord
+                    modified_char += 26 if modified_char < 'a'.ord
+                else
+                    modified_char -= 26 if modified_char > 'Z'.ord 
+                    modified_char += 26 if modified_char < 'A'.ord
+                end
+                output_str += modified_char.chr
+            else
+                output_str += char
+            end
+        end
+        if args.length == 0
+            return output_str
+        else
+            output_file = File.open(args[0], 'w')
+            output_file.write(output_str)
+            output_file.close
+            return 0
+        end
     end
 end
 
-input_k = ""
-output_str = ""
+cipherInstance = CaesarCipher.new
+inputFile = File.open('CaesarCipherDecrypted.txt')
+cipherInstance.perform('e', 2, inputFile, 'CaesarCipherEncrypted.txt')
 
-until input_k =~ /\d/
-    print "Offset for Caesar cypher: "
-    input_k = gets.chomp
-end
-
-input_str.each_char do |char|
-    if char =~ /[a-z]|[A-Z]/
-        if mode == "1"
-            modified_char = char.ord + input_k.to_i
-        else
-            modified_char = char.ord - input_k.to_i
-        end
-        if char =~ /[a-z]/
-            modified_char -= 26 if modified_char > "z".ord
-            modified_char += 26 if modified_char < "a".ord
-        else
-            modified_char -= 26 if modified_char > "Z".ord 
-            modified_char += 26 if modified_char < "A".ord
-        end
-        output_str += modified_char.chr
-    else
-        output_str += char
-    end
-end
-
-puts "Output: #{output_str}"
-
-if mode == "1"
-    puts "Writing to CaesarCipherEncrypted.txt..."
-    File.write("CaesarCipherEncrypted.txt", output_str)
-else
-    puts "Writing to CaesarCipherDecrypted.txt..."
-    File.write("CaesarCipherDecrypted.txt", output_str)
-end
